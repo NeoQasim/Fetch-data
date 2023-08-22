aef2d4b733db545b86ab0f22eba80cc319de77b3
NeoQasim <139628391+NeoQasim@users.noreply.github.com>
2023-08-22 23:32:40 +0500

    fecthed data from api

---
+++ README.md
@@ -0,0 +1,3 @@
+### little info about app
+
+This is a simple practice project to fetch data from a dummyapi
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Api_Data from './api_fetch/Api_Data'
+
+const App = () => {
+  return (
+    <>
+      <div>App  working fine </div>
+      <Api_Data />
+
+    </>
+  )
+
+}
+export default App
